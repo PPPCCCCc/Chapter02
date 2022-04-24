bfae4bc93e6cc8fcc3e391983290c0bf3f66dd05
qkrcksgmlxd <qkrcksgmlxd@gmail.com>
2022-04-24 21:46:53 +0900

    1

--- src/com/javaex/ex04/Point.java
+++ src/com/javaex/ex04/Point.java
@@ -1,5 +1,7 @@
 package com.javaex.ex04;
 
+import java.awt.Desktop.Action;
+
 public class Point {
 
 	// 필드
@@ -7,7 +9,18 @@ public class Point {
 	private int y;
 
 	// 생성자
-
+	public Point() {
+		//메모리에 올리는일
+	}
+	public Point(int x) {
+		//메모리에 올리는일
+		this.x = x;
+	}
+	public Point(int x, int y) {
+	//******메모리에 올리는 일(클래스로 인스턴스화)
+		this.x = x;
+		this.y = y;
+	}
 	// 메소드 -gs
 	public void setX(int x) {
 		this.x = x;
@@ -28,5 +41,14 @@ public class Point {
 	// 메소드 -일반
 	public void draw() {
 		System.out.printf("점[x=%d, y=%d]을 그렸습니다.\n",x,y);
+	}
+	
+	
+	public void draw(boolean action) {
+		if(action == true) {
+			System.out.printf("점[x=%d, y=%d]을 그렸습니다.\n",x,y);
+		}else {
+			System.out.printf("점[x=%d, y=%d]을 지웠습니다.\n",x,y);
+		}
 	}
 }
--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -11,8 +11,22 @@ public class PointApp {
 		numy.setX(10);
 		numy.setY(23);
 		
+		Point numj = new Point(10,20);
+		
+		Point numi = new Point(100);
+		numi.setY(200);
+		
+		//y값을 먼저 넣고
+		//setter x값을 넣는다
+		//안됨
+		Point numk = new Point(500);
+ 		
 		numx.draw();
 		numy.draw();
+		numj.draw();
+		
+		numx.draw(true);
+		numx.draw(false);
 	}
 
 }
